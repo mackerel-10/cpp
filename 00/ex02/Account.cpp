7351f6c3b8c5b7fd159c8d29c9d4f8d039e1fd4c
Sujin Jeon <sujeon@sujin.local>
2021-11-24 20:46:42 +0900

    11.24: ex02하는중

--- 00/ex02/Account.cpp
+++ 00/ex02/Account.cpp
@@ -6,15 +6,22 @@
 /*   By: sujeon <sujeon@student.42.kr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 21:22:50 by sujeon            #+#    #+#             */
-/*   Updated: 2021/11/09 15:21:57 by sujeon           ###   ########.fr       */
+/*   Updated: 2021/11/24 20:14:10 by sujeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Account.hpp"
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
 
 // 생성자, 소멸자
 Account::Account(int initial_deposit)
 {
+	this->_displayTimestamp();
 	// created
 }
 
@@ -23,21 +30,61 @@ Account::Account(void)
 	
 }
 
-Account::~Account( void )
+Account::~Account(void)
 {
 	// closed
 }
 
-int		Account::getNbAccounts( void ){}
-int		Account::getTotalAmount( void ){}
-int		Account::getNbDeposits( void ){}
-int		Account::getNbWithdrawals( void ){}
-void	Account::displayAccountsInfos( void ){}
+// GET
+// static 멤버 함수는 오직 static 멤버 변수에 접근하거나 static 멤버 함수만 호출할 수 있다.
+int		Account::getNbAccounts(void)
+{
+	return (Account::_nbAccounts);
+}
 
-void	Account::makeDeposit( int deposit ){}
-bool	Account::makeWithdrawal( int withdrawal ){}
-int		Account::checkAmount( void ) const{}
-void	Account::displayStatus( void ) const{}
+int		Account::getTotalAmount( void )
+{
+	return (Account::_totalAmount);
+}
 
-void	Account::_displayTimestamp( void ){}
+int		Account::getNbDeposits( void )
+{
+	return (Account::_totalNbDeposits);
+}
 
+int		Account::getNbWithdrawals( void )
+{
+	return(Account::_totalNbWithdrawals);
+}
+
+void	Account::displayAccountsInfos( void )
+{
+	std::cout << "displayAccountsInfos" << std::endl;
+}
+
+void	Account::makeDeposit( int deposit )
+{
+	std::cout << "makeDeposit" << std::endl;
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	std::cout << "makeWithdrawal" << std::endl;
+	return true;
+}
+
+int		Account::checkAmount(void) const
+{
+	std::cout << "checkAmount" << std::endl;
+	return 1;
+}
+
+void	Account::displayStatus(void) const
+{
+	std::cout << "displayStatus" << std::endl;
+}
+
+void	Account::_displayTimestamp(void)
+{
+	std::cout << "_displayTimestamp" << std::endl;
+}
