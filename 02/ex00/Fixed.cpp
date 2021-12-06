6c8d31fc9b7acafd60bfd3b965a4c90543a0beb5
Sujin Jeon <sujeon@sujin.local>
2021-12-06 23:20:25 +0900

    1206: 02 ex00 끝

--- 02/ex00/Fixed.cpp
+++ 02/ex00/Fixed.cpp
@@ -6,27 +6,49 @@
 /*   By: sujeon <sujeon@student.42.kr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 21:57:05 by sujeon            #+#    #+#             */
-/*   Updated: 2021/12/06 20:56:44 by sujeon           ###   ########.fr       */
+/*   Updated: 2021/12/06 23:11:15 by sujeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
+// 항상 8로 고정
 const int Fixed::_fractional_bit = 8;
 
-/* 생성자, 복사 연산자,  */
-Fixed::Fixed()
-{
+/* 생성자, 복사 연산자, 할당 연산자 오버로딩, 소멸자*/
+// constructor(생성자)
+Fixed::Fixed(){
+	std::cout << "Default constructor called" << std::endl;
 	_fixed_point_value = 0;
 }
 
-Fixed::~Fixed()
-{
-	
+// copy constructor(복사 생성자)
+Fixed::Fixed(const Fixed &object){
+	std::cout << "Copy constructor called" << std::endl;
+	*this = object;
+	// _fixed_point_value = object._fixed_point_value;
 }
 
-Fixed(const Fixed &copy);	// copy constructor(복사 생성자)
-Fixed &operator = (const Fixed &copy);	// assignation operator overload(할당 연산자 오버로딩)
-		int getRawBits(void) const;
-		void setRawBits(int const raw);
+// assignation operator overload(할당 연산자 오버로딩)
+// 객체 = 객체
+Fixed & Fixed::operator = (const Fixed &object){
+	std::cout << "Assignation member function called" << std::endl;
+	_fixed_point_value = object.getRawBits();
+	return *this;
+}
+
+// destructor(소멸자)
+Fixed::~Fixed(){
+	std::cout << "Destructor called" << std::endl;
+}
+
+/* Public */
+int Fixed::getRawBits(void) const{
+	std::cout << "getRawBits member function called" << std::endl;
+	return _fixed_point_value;
+}
+
+void Fixed::setRawBits(int const raw){
+	_fixed_point_value = raw;
+}
 
--- 02/ex00/Fixed.hpp
+++ 02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: sujeon <sujeon@student.42.kr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 21:57:09 by sujeon            #+#    #+#             */
-/*   Updated: 2021/12/06 20:56:58 by sujeon           ###   ########.fr       */
+/*   Updated: 2021/12/06 22:56:27 by sujeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,6 @@
 
 #include <iostream>
 
-/*
-	Orthodox canonical
-	- Default constructor(기본 생성자)
-	- Copy constructor(복사 생성자)
-*/
-
 class Fixed
 {
 	private:
@@ -28,10 +22,12 @@ class Fixed
 		static const int	_fractional_bit;
 	
 	public:
+		// Orthodox Canonical
 		Fixed();	// default constructor(기본 생성자)
 		~Fixed();	// destructor(소멸자)
-		Fixed(const Fixed &copy);	// copy constructor(복사 생성자)
-		Fixed &operator = (const Fixed &copy);	// assignation operator overload(할당 연산자 오버로딩)
+		Fixed(const Fixed &object);	// copy constructor(복사 생성자)
+		Fixed & operator = (const Fixed &object);	// assignation operator overload(할당 연산자 오버로딩)
+
 		int getRawBits(void) const;
 		void setRawBits(int const raw);
 };
