5142a32734fa7dad03eb70d572f8ed0ec1f6a943
Sujin Jeon <sujeon@sujin.local>
2021-12-07 16:37:16 +0900

    1207: 02 ex02 main 수정

--- 02/ex02/main.cpp
+++ 02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sujeon <sujeon@student.42.kr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 13:01:15 by sujeon            #+#    #+#             */
-/*   Updated: 2021/12/07 16:31:32 by sujeon           ###   ########.fr       */
+/*   Updated: 2021/12/07 16:36:36 by sujeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,60 +21,14 @@ int	main( void ) {
 	Fixed	a;
 	Fixed	const b(Fixed(5.05f) * Fixed(2));
 
-	{
-		// 과제 예시
-		std::cout << "*Subject*" << std::endl;
-		std::cout << a << std::endl;
-		std::cout << ++a << std::endl;
-		std::cout << a << std::endl;
-		std::cout << a++ << std::endl;
-		std::cout << a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
 	
-		std::cout << b << std::endl;
+	std::cout << b << std::endl;
 		
-		std::cout << Fixed::max( a, b ) << std::endl;
-
-		std::cout << std::endl;
-	}
-	{
-		// 비교 연산자
-		std::cout << "*Comparison operator" << std::endl;
-		std::cout << "a: " << a << std::endl;
-		std::cout << "b: " << b << std::endl;
-
-		std::cout << std::boolalpha << "a > b: " << (a > b) << std::endl;
-		std::cout << std::boolalpha << "a < b: " << (a < b) << std::endl;
-		std::cout << std::boolalpha << "a <= b: " << (a <= b) << std::endl;
-		std::cout << std::boolalpha << "a >= b: " << (a >= b) << std::endl;
-		std::cout << std::boolalpha << "a == b: " << (a == b) << std::endl;
-		std::cout << std::boolalpha << "a != b: " << (a != b) << std::endl;
-		std::cout << std::endl;
-
-		// 산술 연산자
-		std::cout << "*Arithmetic operator*" << std::endl;
-		std::cout << "a: " << a << std::endl;
-		std::cout << "b: " << b << std::endl;
-
-		std::cout << "+ " << a + b << std::endl;
-		std::cout << "- " << a - b << std::endl;
-		std::cout << "* " << a * b << std::endl;
-		std::cout << "/ " << a / b << std::endl;
-		std::cout << std::endl;
-
-		// 단항 연산자(증감 연산자)
-		std::cout << "*pre/post-increment/decrement*" << std::endl;
-		std::cout << "a: " << a << std::endl;
-		std::cout << "++a: " << ++a << std::endl;
-		std::cout << "a: " << a << std::endl;
-		std::cout << "a++: " << a++ << std::endl;
-		std::cout << "a: " << a << std::endl;
-		std::cout << std::endl;
-
-		// min, max
-		std::cout << "a: " << a << std::endl;
-		std::cout << "b: " << b << std::endl;
-		std::cout << "min: " << Fixed::min(a, b) << std::endl;
-		std::cout << "max: " << Fixed::max(a, b) << std::endl;
-	}
+	std::cout << Fixed::max( a, b ) << std::endl;
 	return 0;
 }
